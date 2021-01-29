01a5232a854343e43b804f715942eb995e06ea4f
Cristina M <mcristinamotoc@gmail.com>
2021-01-29 18:17:08 +0100

    Nivell 1 - Ex.1:  API call with axios

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, { useState} from "react";
+import { Container, Jumbotron, Button} from "bootstrap-4-react";
+import axios from "axios";
 
-function App() {
+
+
+export default () => {
+  const [joke, setJoke] = useState("");
+  const getJoke = () => {
+    axios
+      .get("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        setJoke(response.data.joke);
+      });
+  };
+
   return (
-    <div >
-   
-    </div>
-  );
-}
+    <Container >
+      <h1 class="text-center text-info my-3">Web D'Acudits</h1>
+      <div class="wheather mx-2" id="wheather"></div>
 
-export default App;
+      <Jumbotron>
+        <h3 class=" text-center" my-4>Preparat per riure? &#129315;</h3>
+
+         {joke}
+
+        <Button
+          info
+          as="input"
+          type="submit"
+          value="Següent acudit"
+          display="flex"
+          alignItems="center"
+          my="4"
+          onClick={getJoke}>
+          </Button>
+       
+      
+      </Jumbotron>
+
+    </Container>
+  );
+};
