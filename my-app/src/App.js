3bb40167aed06346e4123444522e8c20974ae793
Cristina M <mcristinamotoc@gmail.com>
2021-01-29 21:34:22 +0100

    Nivell 2 - Ex. 2: Project layout and styling

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import React, { useState} from "react";
-import { Container, Jumbotron, Button} from "bootstrap-4-react";
+import React, { useState } from "react";
+import { Container, Jumbotron, Button } from "bootstrap-4-react";
 import axios from "axios";
 
-
-
 export default () => {
+
+  // This function will load a request from the API and show it to the user when he clicks on the  button.
+  
   const [joke, setJoke] = useState("");
   const getJoke = () => {
     axios
@@ -18,30 +19,48 @@ export default () => {
       });
   };
 
+
+  // Set Background Image
+  const backgroundImg = {
+    background:
+      "url('https://i.postimg.cc/Twhcyp8B/pexels-helena-lopes-708440.jpg') no-repeat",
+    backgroundSize: "cover",
+    height: "100vh",
+    minHeight: "100vh",
+  };
+
   return (
-    <Container >
-      <h1 class="text-center text-info my-3">Web D'Acudits</h1>
-      <div class="wheather mx-2" id="wheather"></div>
+    // Set the background image, title, joke space and next joke button.
 
-      <Jumbotron>
-        <h3 class=" text-center" my-4>Preparat per riure? &#129315;</h3>
+    <div style={backgroundImg}>
+      <Container my="2">
+        <h1 class="text-center text-info my-3">Web D'Acudits</h1>
+        <div class="wheather mx-2" id="wheather"></div>
 
-         {joke}
+        <Jumbotron
+          className="w-50 h-25"
+          bg="light"
+          text="center"
+          mx="auto"
+          mb="3">
 
-        <Button
-          info
-          as="input"
-          type="submit"
-          value="Següent acudit"
-          display="flex"
-          alignItems="center"
-          my="4"
-          onClick={getJoke}>
-          </Button>
-       
-      
-      </Jumbotron>
+          <h4 className=" text-center mb-4"> Preparat per riure? &#129315;</h4>
 
-    </Container>
+          <div class="text-center" my-3> {joke} </div>
+
+          <Button
+            info
+            as="input"
+            type="submit"
+            value="Següent acudit"
+            mt="4"
+            align="centered"
+            onClick={getJoke}>
+
+            </Button>
+
+        </Jumbotron>
+      </Container>
+    </div>
   );
 };
